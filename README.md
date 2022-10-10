246fa3136e62b74ffc2a98fb6c2809d721e370d9
jvpalves <jvp.alves@hotmail.com>
2022-10-10 16:49:53 -0300

    adding template files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # A031 - Template
 
+Utilize o conteúdo da pasta `template` para executar o exercício de fixação. Rode `npm install` para executar a instalação do React antes de tentar rodar o projeto com `npm run start`.
+
 [Template no CodeSandbox](https://codesandbox.io/s/template-exercicio-de-aula-componentes-qwoj0m)
---
+++ template/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import logo from "./img/logo.png";
+
+export default function App() {
+  const onClickBotao = () => {
+    alert("Botão foi clicado!");
+  };
+
+  return (
+    <div className="App">
+      <h1>Aprenda React na Labenu!</h1>
+      <div>
+        <div>
+          <img className={"Logo"} src={logo} alt={"logo"} />
+        </div>
+        <div>
+          <a href="https://labenu.com.br">Site da Labenu</a>
+        </div>
+        <div>
+          <button onClick={onClickBotao}> Clique aqui! </button>
+        </div>
+      </div>
+    </div>
+  );
+}
